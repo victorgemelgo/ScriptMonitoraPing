301f51035d283f4836daa3b85bd22a412c09306e
Victor B. Gemelgo <32135865+victorgemelgo@users.noreply.github.com>
2024-07-02 20:00:59 -0300

    Add files via upload

---
+++ Monitora_Ping.ps1
@@ -0,0 +1,14 @@
+﻿$hostname = "8.8.8.8" #Colocar o Endereço a ser monitorado
+
+while ($true) {
+    $pingResult = Test-Connection -ComputerName $hostname -Count 1 -Quiet
+    $currentDateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+    if ($pingResult) {
+        Write-Output "$currentDateTime - $hostname Esta Respondendo"
+    } else {
+        Write-Output "$currentDateTime - $hostname Nao esta Respondendo"
+    }
+
+    Start-Sleep -Seconds 5 # Ajuste o intervalo conforme necessário
+}
